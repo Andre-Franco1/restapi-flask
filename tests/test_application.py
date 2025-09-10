de4562351b13e1d4c1edeb062618070867e97709
Andre-Franco1 <andrefranco3172@gmail.com>
2025-09-10 19:09:36 -0300

    test: testing for get user

--- tests/test_application.py
+++ tests/test_application.py
@@ -12,20 +12,20 @@ class TestApplication():
     @pytest.fixture
     def valid_user(self):
         return {
-            "first_name": "name",
-            "last_name": "surname",
+            "first_name": "John",
+            "last_name": "Doe",
             "cpf": "734.004.100-12",
-            "email": "contato@namesurname.com",
+            "email": "johndoe@mail.com",
             "birth_date": "2001-01-01"
         }
 
     @pytest.fixture
     def invalid_user(self):
         return {
-            "first_name": "name",
-            "last_name": "surname",
-            "cpf": "734.004.100-11",
-            "email": "contato@namesurname.com",
+            "first_name": "John",
+            "last_name": "Doe",
+            "cpf": "734.004.100-11",            # Invalid CPF
+            "email": "johndoe@mail.com",
             "birth_date": "2001-01-01"
         }
 
@@ -39,5 +39,20 @@ class TestApplication():
         assert b"successfully" in response.data
 
         response = client.post('/user', json=invalid_user)
+        assert response.status_code == 400
+        assert b"invalid" in response.data
+
+    def test_get_user(self, client, valid_user, invalid_user):
+        response = client.get('/user/%s' % valid_user["cpf"])
         assert response.status_code == 200
-        assert b"successfully" in response.data
+        assert response.json[0]["first_name"] == "John"
+        assert response.json[0]["last_name"] == "Doe"
+        assert response.json[0]["cpf"] == "734.004.100-12"
+        assert response.json[0]["email"] == "johndoe@gmail.com"
+
+        birth_date = response.json[0]["birth_date"]["$date"]
+        assert birth_date == "2001-01-01T00:00:00Z"
+
+        response = client.get('/user/%s' % invalid_user["cpf"])
+        assert response.status_code == 400
+        assert b"User does not exist in database!" in response.data
